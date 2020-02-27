f0c3ae6be9805be296be562779ce5d82123c5d97
Arsalan <airavani2018@ada.edu.az>
2020-02-27 20:55:04 +0100

    alpha 0.1

--- AppsFactory Music/Controllers/AlbumDetailsViewController.swift
+++ AppsFactory Music/Controllers/AlbumDetailsViewController.swift
@@ -22,6 +22,7 @@ class AlbumDetailsViewController: UIViewController {
     @IBOutlet weak var albumImageView: UIImageView!
     @IBOutlet weak var albumNameAndArtistLabel: MarqueeLabel!
     
+    @IBOutlet weak var saveAlbumMarkImageView: UIImageView!
     @IBOutlet weak var moreButton: UIButton!
     @IBOutlet weak var tracksTableView: UITableView!
     
@@ -44,42 +45,34 @@ class AlbumDetailsViewController: UIViewController {
             guard let self = self else { return }
             RealmManager.shared.deleteAlbumFromRealm(withID: album.id)
             self.presentHUD(with: "Deleted")
+            self.saveAlbumMarkImageView.isHidden = true
+            self.navigationController?.popViewController(animated: true)
         }
         
         let saveAction = UIAlertAction(title: "Save", style: .default) { [weak self] action in
             guard let self = self else { return }
             RealmManager.shared.saveAlbumToRealm(album, tracks: self.tracks)
+            self.saveAlbumMarkImageView.isHidden = false
             self.presentHUD(with: "Saved")
         }
         
         RealmManager.shared.loadAlbumsFromRealm { albums in
-            
             if albums.filter({$0.id == album.id}).isEmpty {
                 deleteAction.isEnabled = false
             } else {
                 saveAction.isEnabled = false
             }
-            
-            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
-            
-            optionMenu.addAction(deleteAction)
-            optionMenu.addAction(saveAction)
-            optionMenu.addAction(cancelAction)
-            
-            self.present(optionMenu, animated: true)
         }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        
+        optionMenu.addAction(deleteAction)
+        optionMenu.addAction(saveAction)
+        optionMenu.addAction(cancelAction)
+        
+        self.present(optionMenu, animated: true)
     }
     
-    private func presentHUD(with message: String) {
-        let hud = JGProgressHUD(style: .dark)
-        hud.indicatorView = JGProgressHUDSuccessIndicatorView()
-        hud.vibrancyEnabled = true
-        hud.interactionType = .blockNoTouches
-        hud.isUserInteractionEnabled = false
-        hud.textLabel.text = message
-        hud.show(in: self.view)
-        hud.dismiss(afterDelay: 1.0)
-    }
     
     private func setupDetails() {
         
@@ -97,6 +90,7 @@ class AlbumDetailsViewController: UIViewController {
                         self.colors = colors
                         
                         self.albumNameAndArtistLabel.textColor = colors?.detail
+                        self.moreButton.tintColor = colors?.primary
                         self.albumImageView.fadeView(style: .bottom, percentage: 0.5, bottomColor: colors?.background ?? .black)
                         self.tracksTableView.reloadData()
                         
@@ -114,6 +108,11 @@ class AlbumDetailsViewController: UIViewController {
             self.tracksTableView.reloadData()
         }
         
+        RealmManager.shared.loadAlbumsFromRealm { [weak self] albums in
+            guard let self = self else { return }
+            self.saveAlbumMarkImageView.isHidden = albums.filter({$0.id == album.id}).isEmpty
+        }
+        
     }
     
     private func setupLayout() {
@@ -122,16 +121,36 @@ class AlbumDetailsViewController: UIViewController {
             albumNameAndArtistLabel.bottomAnchor.constraint(equalTo: tracksTableView.topAnchor, constant: -10),
             albumNameAndArtistLabel.heightAnchor.constraint(equalToConstant: 50),
             albumNameAndArtistLabel.leadingAnchor.constraint(equalTo: tracksTableView.leadingAnchor, constant: 20),
-            albumNameAndArtistLabel.trailingAnchor.constraint(equalTo: moreButton.leadingAnchor, constant: -10)
+            albumNameAndArtistLabel.trailingAnchor.constraint(equalTo: saveAlbumMarkImageView.leadingAnchor, constant: -5)
         ])
         
         moreButton.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            moreButton.bottomAnchor.constraint(equalTo: tracksTableView.topAnchor, constant: -10),
+            moreButton.bottomAnchor.constraint(equalTo: tracksTableView.topAnchor, constant: -12),
             moreButton.heightAnchor.constraint(equalToConstant: 50),
             moreButton.widthAnchor.constraint(equalToConstant: 50),
-            moreButton.trailingAnchor.constraint(equalTo: tracksTableView.trailingAnchor, constant: -10)
+            moreButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10)
         ])
+        
+        saveAlbumMarkImageView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            saveAlbumMarkImageView.centerYAnchor.constraint(equalTo: moreButton.centerYAnchor),
+            saveAlbumMarkImageView.heightAnchor.constraint(equalToConstant: 30),
+            saveAlbumMarkImageView.widthAnchor.constraint(equalToConstant: 30),
+            saveAlbumMarkImageView.trailingAnchor.constraint(equalTo: moreButton.leadingAnchor)
+        ])
+        
+    }
+    
+    private func presentHUD(with message: String) {
+        let hud = JGProgressHUD(style: .dark)
+        hud.indicatorView = JGProgressHUDSuccessIndicatorView()
+        hud.vibrancyEnabled = true
+        hud.interactionType = .blockNoTouches
+        hud.isUserInteractionEnabled = false
+        hud.textLabel.text = message
+        hud.show(in: self.view)
+        hud.dismiss(afterDelay: 1.0)
     }
     
 }
